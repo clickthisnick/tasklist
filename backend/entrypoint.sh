aa21f386524fb75a959077722a4ef22085e32b97
Nick Smith <admin@clickthisnick.com>
2018-10-16 20:45:36 -0400

    Replace typescript backend with django framework

---
+++ backend/entrypoint.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+sleep 15
+
+# wait for MySQL to be available so we can run flyway migrations
+until nc -z -v -w30 mariadb 3306
+do
+  echo "Waiting for database connection..."
+  # wait for 5 seconds before check again
+  sleep 5
+done
+
+python3 manage.py runserver 0.0.0.0:8000
